83db14b1dc9ca58fee6916aa85c0ee5277f9dce8
Mert Batmazoglu <mertbatmazoglu@gmail.com>
2022-08-17 19:37:08 +0300

    Import services in test suite to execute listeners

---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+import LoggerService from "services/LoggerService";
+import UserService from "services/UserService";
+import VerificationService from "services/VerificationService";
+
+export { LoggerService, UserService, VerificationService };
--- src/tests/testSetup.js
+++ src/tests/testSetup.js
@@ -1,4 +1,7 @@
 import TestDB from "tests/TestDB";
+// Import services so event listeners can be initialized
+// eslint-disable-next-line no-unused-vars
+import Services from "services";
 
 beforeAll(async () => {
   await TestDB.initialize();
