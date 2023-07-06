5b8d3165f31997efec1bb05f7924e825580e8564
Mert Batmazoglu <mertbatmazoglu@gmail.com>
2023-07-06 14:11:43 +0300

    Change jest settings

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>/src/tests/testSetup.js"],
   verbose: true,
-  modulePathIgnorePatterns: ["<rootDir>/build/"],
-
-  // Jest jsDom environment does not work without ignoring below pattern
-  transformIgnorePatterns: ["/node_modules/(?!uuid)"],
 };
